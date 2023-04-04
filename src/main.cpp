b7cc239a67c7576076ebe93f8150996a53157e92
Grayson <89096786+pieshower@users.noreply.github.com>
2023-04-04 17:53:01 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,8 @@
 
 CAN_message_t rxMsg;
 
-uint8_t temp[8];
+uint8_t temp1[8];
+uint8_t temp2[8];
 
 
 void setup()
@@ -24,7 +25,19 @@ void loop()
     if(ReadCAN(rxMsg))
     {
 
-        memcpy(rxMsg.buf, temp, rxMsg.len);
+        if (rxMsg.id == 0xB0)
+        {
+
+            memcpy(temp1, rxMsg.buf, rxMsg.len);
+
+        }
+        else if (rxMsg.id == 0xB1)
+        {
+
+            memcpy(temp2, rxMsg.buf, rxMsg.len);
+
+        }
+
 
     }
 
@@ -33,10 +46,21 @@ void loop()
     for (int i = 0; i < rxMsg.len; i++)
     {
 
-        Serial.print("Temp ");
+        Serial.print("Temp1 ");
         Serial.print(i);
         Serial.print(": ");
-        Serial.print(temp[i]);
+        Serial.print(temp1[i]);
+        Serial.println();
+
+    }
+
+    for (int i = 0; i < rxMsg.len; i++)
+    {
+
+        Serial.print("Temp2 ");
+        Serial.print(i);
+        Serial.print(": ");
+        Serial.print(temp2[i]);
         Serial.println();
 
     }
